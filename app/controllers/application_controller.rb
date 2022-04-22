1db1fec3dff7550ce3472cbb47368808d3e79794
jericho44 <ricoding17@gmail.com>
2022-04-23 02:27:00 +0700

    add username for registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+before_action :configure_permitted_parameters, if: :devise_controller?
+attr_accessor :username
+
+
+    protected
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:email, :password,:username) }
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation,:username) }
+        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :password_confirmation,:username) }
+    end
+
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_22_130436) do
+ActiveRecord::Schema.define(version: 2022_04_22_183550) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -37,8 +37,10 @@ ActiveRecord::Schema.define(version: 2022_04_22_130436) do
     t.integer "role", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "menus", "categories"
