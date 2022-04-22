bbfe814e4969150b3ba6b9421252128797314118
jericho44 <ricoding17@gmail.com>
2022-04-22 19:56:18 +0700

    add schafold category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_22_122844) do
+ActiveRecord::Schema.define(version: 2022_04_22_125430) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
